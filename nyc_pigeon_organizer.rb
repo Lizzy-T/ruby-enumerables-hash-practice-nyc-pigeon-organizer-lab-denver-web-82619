219c20a616d5897a564feaf46af9e7b5fff57212
Lizzy Tong <lizzy.tong33@gmail.com>
2019-08-15 23:52:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,25 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
+  p "++++++++++ start ++++++++++++++"
   data.each do |key, value|
+    p "+++++ data.each +++++"
+    p "key : #{key}"
+    p "value : #{value}"
     value.each do |new_value, names|
+      p " ++++ value.each +++++"
+      p "new_value : #{new_value}"
+      p "names : #{names}"
       names.each do |name|
-
+        p "name #{name}"
         if !new_hash[name]
-          new_hash[name] = {}
+        p  new_hash[name] = {}
         end
 
         if !new_hash[name][key]
-          new_hash[name][key] = []
+        p  new_hash[name][key] = []
         end
 
-        new_hash[name][key] << new_value.to_s
+    p    new_hash[name][key] << new_value.to_s
 
       end
     end
